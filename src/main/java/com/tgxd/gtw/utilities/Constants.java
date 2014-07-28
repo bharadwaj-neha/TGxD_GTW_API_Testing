cda73153897a8818e680cf652cacf6030eb19985
bharadwaj-neha <neha16bharadwaj@gmail.com>
2014-07-28 17:07:12 +0530

    commit file

--- src/main/java/com/tgxd/gtw/utilities/Constants.java
+++ src/main/java/com/tgxd/gtw/utilities/Constants.java
@@ -9,20 +9,23 @@ public interface Constants {
 		
 		String provider = "/2/";
 		
-	
+		String defaultLocale = "en-US";
 		
 		long passStatusCode = 200;
-		String serviceId ="73625";
 		String celebrityIdentifierList = "CelebrityIdentifierList";
 		String[] nameGetFavoritesCelebrityIds={"101229","100668"};	
 		
-		
+		String burbankServiceId = "896643";
+		String claroServiceId = "68337";
+		String tulsaServiceId ="73625";
 		
 		//Name Resource
 		String nameName = "/Name/Name/";
 		String nameCredits = "/Name/Credits/";
 		String nameAirings = "/Name/Airings/";
 		String nameGetFavorites = "/Name/GetFavorites/";
+		
+		
 		
 		
 		
